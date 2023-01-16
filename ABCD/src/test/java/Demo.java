0222806ef11dbcefefd01bf2b4b7a4ae2796cd9a
yjema <yjema@New2023.lan>
2023-01-15 19:50:19 -0600

    2nd

--- ABCD/src/test/java/Demo.java
+++ ABCD/src/test/java/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello yared");
-
+		System.out.println("hello mf");
 	}
 
 }
